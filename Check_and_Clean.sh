43fc5ce09b072c9086aeebbc49f6496206c4ac63
RMS <rms@gmn.uwo.ca>
2023-06-11 21:38:16 +0000

    Update date and byte count

--- Check_and_Clean.sh
+++ Check_and_Clean.sh
@@ -11,7 +11,7 @@ night_dir="$(basename "$1")"
 station=${night_dir:0:6}
 OUTFILE=$data_dir"/"${station}_"fits_counts.txt"
 
-printf '\nCheck_and_Clean.sh, revised 23-May, 2023, 7968 bytes,'
+printf '\nCheck_and_Clean.sh, revised 11-Jun, 2023, 7978 bytes,'
 printf ' was called with\nArg (directory) = %s \n' "$1"
 printf 'This script writes results to %s \n' $OUTFILE
 printf ' and can delete older files to make room for more capture directories\n\n'
